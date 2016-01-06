bf616a4af8e1e04cd24acb875ec867fd555297a8
Systems123 <Systems123@CM-SYSTEMS>
2016-01-06 15:11:07 +0530

    First Change

---
+++ basic programs/src/Repository/Repo.java
@@ -0,0 +1,17 @@
+package Repository;
+
+public class Repo {
+
+	public static void main(String[] args) {
+		Salesdata data= new Salesdata();
+		display();
+		data.display();
+		
+	}
+	private static void display()
+	{
+		System.out.println("test");
+		System.out.println("please ");
+	}
+
+}
